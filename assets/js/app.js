ddac04a64a1c7a7c460514df6f7d341919f487b6
Neofyt <gtouzeau17@gmail.com>
2013-06-26 12:13:13 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,662 @@
+var table = {
+	hydrogen: {
+		n: 1,
+		s: 'H',
+		w: 1.00794,
+		c: 'onm'
+	},
+	helium: {
+		n: 2,
+		s: 'He',
+		w: 4.002602,
+		c: 'ng'
+	},
+	lithium: {
+		n: 3,
+		s: 'Li',
+		w: 6.941,
+		c: 'am'
+	},
+	beryllium: {
+		n: 4,
+		s: 'Be',
+		w: 9.012182,
+		c: 'aem'
+	},
+	boron: {
+		n: 5,
+		s: 'B',
+		w: 10.811,
+		c: 'me'
+	},
+	carbon: {
+		n: 6,
+		s: 'C',
+		w: 12.0107,
+		c: 'onm'
+	},
+	nitrogen: {
+		n: 7,
+		s: 'N',
+		w: 14.0067,
+		c: 'onm'
+	},
+	oxygen: {
+		n: 8,
+		s: 'O',
+		w: 15.9994,
+		c: 'onm'
+	},
+	fluorine: {
+		n: 9,
+		s: 'F',
+		w: 18.9984032
+	},
+	neon: {
+		n: 10,
+		s: 'Ne',
+		w: 20.1797,
+		c: 'ng'
+	},
+	sodium: {
+		n: 11,
+		s: 'Na',
+		w: 22.98976928,
+		c: 'am'
+	},
+	magnesium: {
+		n: 12,
+		s: 'Mg',
+		w: 24.305,
+		c: 'aem'
+	},
+	aluminium: {
+		n: 13,
+		s: 'Al',
+		w: 26.9815386
+	},
+	silicon: {
+		n: 14,
+		s: 'Si',
+		w: 28.0855,
+		c: 'me'
+	},
+	phosphorus: {
+		n: 15,
+		s: 'P',
+		w: 30.973762,
+		c: 'onm'
+	},
+	sulfur: {
+		n: 16,
+		s: 'S',
+		w: 32.065,
+		c: 'onm'
+	},
+	chlorine: {
+		n: 17,
+		s: 'Cl',
+		w: 35.453
+	},
+	argon: {
+		n: 18,
+		s: 'Ar',
+		w: 39.948,
+		c: 'ng'
+	},
+	potassium: {
+		n: 19,
+		s: 'K',
+		w: 39.0983,
+		c: 'am'
+	},
+	calcium: {
+		n: 20,
+		s: 'Ca',
+		w: 40.078,
+		c: 'aem'
+	},
+	scandium: {
+		n: 21,
+		s: 'Sc',
+		w: 44.955912
+	},
+	titanium: {
+		n: 22,
+		s: 'Ti',
+		w: 47.867
+	},
+	vanadium: {
+		n: 23,
+		s: 'V',
+		w: 50.9415
+	},
+	chromium: {
+		n: 24,
+		s: 'Cr',
+		w: 51.9961
+	},
+	manganese: {
+		n: 25,
+		s: 'Mn',
+		w: 54.938045
+	},
+	iron: {
+		n: 26,
+		s: 'Fe',
+		w: 55.845
+	},
+	cobalt: {
+		n: 27,
+		s: 'Co',
+		w: 58.933195
+	},
+	nickel: {
+		n: 28,
+		s: 'Ni',
+		w: 58.6934
+	},
+	copper: {
+		n: 29,
+		s: 'Cu',
+		w: 63.546
+	},
+	zinc: {
+		n: 30,
+		s: 'Zn',
+		w: 65.38
+	},
+	gallium: {
+		n: 31,
+		s: 'Ga',
+		w: 69.723
+	},
+	germanium: {
+		n: 32,
+		s: 'Ge',
+		w: 72.63,
+		c: 'me'
+	},
+	arsenic: {
+		n: 33,
+		s: 'As',
+		w: 74.9216,
+		c: 'me'
+	},
+	selenium: {
+		n: 34,
+		s: 'Se',
+		w: 78.96,
+		c: 'onm'
+	},
+	bromine: {
+		n: 35,
+		s: 'Br',
+		w: 79.904
+	},
+	krypton: {
+		n: 36,
+		s: 'Kr',
+		w: 83.798,
+		c: 'ng'
+	},
+	rubidium: {
+		n: 37,
+		s: 'Rb',
+		w: 85.4678,
+		c: 'am'
+	},
+	strontium: {
+		n: 38,
+		s: 'Sr',
+		w: 87.62,
+		c: 'aem'
+	},
+	yttrium: {
+		n: 39,
+		s: 'Y',
+		w: 88.90585
+	},
+	zirconium: {
+		n: 40,
+		s: 'Zr',
+		w: 91.224
+	},
+	niobium: {
+		n: 41,
+		s: 'Nb',
+		w: 92.90638
+	},
+	molybdenum: {
+		n: 42,
+		s: 'Mo',
+		w: 95.96
+	},
+	technetium: {
+		n: 43,
+		s: 'Tc',
+		w: (98)
+	},
+	ruthenium: {
+		n: 44,
+		s: 'Ru',
+		w: 101.07
+	},
+	rhodium: {
+		n: 45,
+		s: 'Rh',
+		w: 102.9055
+	},
+	palladium: {
+		n: 46,
+		s: 'Pd',
+		w: 106.42
+	},
+	silver: {
+		n: 47,
+		s: 'Ag',
+		w: 107.8682
+	},
+	cadmium: {
+		n: 48,
+		s: 'Cd',
+		w: 112.411
+	},
+	indium: {
+		n: 49,
+		s: 'In',
+		w: 114.818
+	},
+	tin: {
+		n: 50,
+		s: 'Sn',
+		w: 118.71
+	},
+	antimony: {
+		n: 51,
+		s: 'Sb',
+		w: 121.76,
+		c: 'me'
+	},
+	tellurium: {
+		n: 52,
+		s: 'Te',
+		w: 127.6,
+		c: 'me'
+	},
+	iodine: {
+		n: 53,
+		s: 'I',
+		w: 126.90447
+	},
+	xenon: {
+		n: 54,
+		s: 'Xe',
+		w: 131.293,
+		c: 'ng'
+	},
+	caesium: {
+		n: 55,
+		s: 'Cs',
+		w: 132.9054519,
+		c: 'am'
+	},
+	barium: {
+		n: 56,
+		s: 'Ba',
+		w: 137.327,
+		c: 'aem'
+	},
+	lanthanum: {
+		n: 57,
+		s: 'La',
+		w: 138.90547
+	},
+	cerium: {
+		n: 58,
+		s: 'Ce',
+		w: 140.116
+	},
+	praseodymium: {
+		n: 59,
+		s: 'Pr',
+		w: 140.90765
+	},
+	neodymium: {
+		n: 60,
+		s: 'Nd',
+		w: 144.242
+	},
+	promethium: {
+		n: 61,
+		s: 'Pm',
+		w: (145)
+	},
+	samarium: {
+		n: 62,
+		s: 'Sm',
+		w: 150.36
+	},
+	europium: {
+		n: 63,
+		s: 'Eu',
+		w: 151.964
+	},
+	gadolinium: {
+		n: 64,
+		s: 'Gd',
+		w: 157.25
+	},
+	terbium: {
+		n: 65,
+		s: 'Tb',
+		w: 158.92535
+	},
+	dysprosium: {
+		n: 66,
+		s: 'Dy',
+		w: 162.5
+	},
+	holmium: {
+		n: 67,
+		s: 'Ho',
+		w: 164.93032
+	},
+	erbium: {
+		n: 68,
+		s: 'Er',
+		w: 167.259
+	},
+	thulium: {
+		n: 69,
+		s: 'Tm',
+		w: 168.93421
+	},
+	ytterbium: {
+		n: 70,
+		s: 'Yb',
+		w: 173.054
+	},
+	lutetium: {
+		n: 71,
+		s: 'Lu',
+		w: 174.9668
+	},
+	hafnium: {
+		n: 72,
+		s: 'Hf',
+		w: 178.49
+	},
+	tantalum: {
+		n: 73,
+		s: 'Ta',
+		w: 180.94788
+	},
+	tungsten: {
+		n: 74,
+		s: 'W',
+		w: 183.84
+	},
+	rhenium: {
+		n: 75,
+		s: 'Re',
+		w: 186.207
+	},
+	osmium: {
+		n: 76,
+		s: 'Os',
+		w: 190.23
+	},
+	iridium: {
+		n: 77,
+		s: 'Ir',
+		w: 192.217
+	},
+	platinum: {
+		n: 78,
+		s: 'Pt',
+		w: 195.084
+	},
+	gold: {
+		n: 79,
+		s: 'Au',
+		w: 196.966569
+	},
+	mercury: {
+		n: 80,
+		s: 'Hg',
+		w: 200.59
+	},
+	thallium: {
+		n: 81,
+		s: 'Tl',
+		w: 204.3833
+	},
+	lead: {
+		n: 82,
+		s: 'Pb',
+		w: 207.2
+	},
+	bismuth: {
+		n: 83,
+		s: 'Bi',
+		w: 208.9804
+	},
+	polonium: {
+		n: 84,
+		s: 'Po',
+		w: (209),
+		c: 'me'
+	},
+	astatine: {
+		n: 85,
+		s: 'At',
+		w: (210)
+	},
+	radon: {
+		n: 86,
+		s: 'Rn',
+		w: (222),
+		c: 'ng'
+	},
+	francium: {
+		n: 87,
+		s: 'Fr',
+		w: (223),
+		c: 'am'
+	},
+	radium: {
+		n: 88,
+		s: 'Ra',
+		w: (226),
+		c: 'aem'
+	},
+	actinium: {
+		n: 89,
+		s: 'Ac',
+		w: (227)
+	},
+	thorium: {
+		n: 90,
+		s: 'Th',
+		w: 232.03806
+	},
+	protactinium: {
+		n: 91,
+		s: 'Pa',
+		w: 231.03588
+	},
+	uranium: {
+		n: 92,
+		s: 'U',
+		w: 238.02891
+	},
+	neptunium: {
+		n: 93,
+		s: 'Np',
+		w: (237)
+	},
+	plutonium: {
+		n: 94,
+		s: 'Pu',
+		w: (244)
+	},
+	americium: {
+		n: 95,
+		s: 'Am',
+		w: (243)
+	},
+	curium: {
+		n: 96,
+		s: 'Cm',
+		w: (247)
+	},
+	berkelium: {
+		n: 97,
+		s: 'Bk',
+		w: (247)
+	},
+	californium: {
+		n: 98,
+		s: 'Cf',
+		w: (251)
+	},
+	einsteinium: {
+		n: 99,
+		s: 'Es',
+		w: (252)
+	},
+	fermium: {
+		n: 100,
+		s: 'Fm',
+		w: (257)
+	},
+	mendelevium: {
+		n: 101,
+		s: 'Md',
+		w: (258)
+	},
+	nobelium: {
+		n: 102,
+		s: 'No',
+		w: (259)
+	},
+	lawrencium: {
+		n: 103,
+		s: 'Lr',
+		w: (262)
+	},
+	rutherfordium: {
+		n: 104,
+		s: 'Rf',
+		w: (267)
+	},
+	dubnium: {
+		n: 105,
+		s: 'Db',
+		w: (268)
+	},
+	seaborgium: {
+		n: 106,
+		s: 'Sg',
+		w: (271)
+	},
+	bohrium: {
+		n: 107,
+		s: 'Bh',
+		w: (272)
+	},
+	hassium: {
+		n: 108,
+		s: 'Hs',
+		w: (270)
+	},
+	meitnerium: {
+		n: 109,
+		s: 'Mt',
+		w: (276)
+	},
+	darmstadtium: {
+		n: 110,
+		s: 'Ds',
+		w: (281)
+	},
+	roentgenium: {
+		n: 111,
+		s: 'Rg',
+		w: (280)
+	},
+	copernicium: {
+		n: 112,
+		s: 'Cn',
+		w: (285)
+	},
+	ununtrium: {
+		n: 113,
+		s: 'Uut',
+		w: (284)
+	},
+	flerovium: {
+		n: 114,
+		s: 'Fl',
+		w: (289)
+	},
+	ununpentium: {
+		n: 115,
+		s: 'Uup',
+		w: (288)
+	},
+	livermorium: {
+		n: 116,
+		s: 'Lv',
+		w: (293)
+	},
+	ununseptium: {
+		n: 117,
+		s: 'Uus',
+		w: (294)
+	},
+	ununoctium: {
+		n: 118,
+		s: 'Uuo',
+		w: (294),
+		c: 'ng'
+	}
+};
+
+
+var li = "<li class='{0}'><span class='symbol'>{1}</span><span class='name'>{2}</span> <span class='f'>#</span> {3}</li>";
+
+
+String.prototype.format = function(){
+	var string = this;
+
+	for (var i = 0, j = arguments.length; i < j; i++) {
+		string = string.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+	}
+
+	return string;
+};
+
+function upperCase(str){
+	return str.charAt(0).toUpperCase() + str.substring(1);
+}
+
+
+
+function display(){
+	tpl = "";
+	for (var prop in table) {
+		var el = table[prop];
+		tpl += li.format(el.c, el.s, upperCase(prop), el.n);
+	}
+	$('ul').html(tpl);
+}
+
+// ===================
+// RUN YOU CLEVER BOY
+// ===================
+
+$(document).ready(function(){
+	display();
+});
