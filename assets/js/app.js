f2beed45fc414262026439250d6c2c79d8f5d657
Neofyt <guillaume@neofyt.com>
2014-04-11 23:37:22 +0200

    New data-structure + multi-filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,831 +1,949 @@
-var table = {
-	Hydrogen: {
+var table = [
+	{
+		p: "Hydrogen",
 		n: 1,
 		s: 'H',
 		w: 1.00794,
 		c: 'onm',
 		e: [1]
 	},
-	Helium: {
+	{
+		p: "Helium",
 		n: 2,
 		s: 'He',
 		w: 4.002602,
 		c: 'ng',
 		e: [2]
 	},
-	Lithium: {
+	{
+		p: "Lithium",
 		n: 3,
 		s: 'Li',
 		w: 6.941,
 		c: 'am',
 		e: [2,1]
 	},
-	Beryllium: {
+	{
+		p: "Beryllium",
 		n: 4,
 		s: 'Be',
 		w: 9.012182,
 		c: 'aem',
 		e: [2,2]
 	},
-	Boron: {
+	{
+		p: "Boron",
 		n: 5,
 		s: 'B',
 		w: 10.811,
 		c: 'me',
 		e: [2,3]
 	},
-	Carbon: {
+	{
+		p: "Carbon",
 		n: 6,
 		s: 'C',
 		w: 12.0107,
 		c: 'onm',
 		e: [2,4]
 	},
-	Nitrogen: {
+	{
+		p: "Nitrogen",
 		n: 7,
 		s: 'N',
 		w: 14.0067,
 		c: 'onm',
 		e: [2,5]
 	},
-	Oxygen: {
+	{
+		p: "Oxygen",
 		n: 8,
 		s: 'O',
 		w: 15.9994,
 		c: 'onm',
 		e: [2,6]
 	},
-	Fluorine: {
+	{
+		p: "Fluorine",
 		n: 9,
 		s: 'F',
 		w: 18.9984032,
 		c: 'ha',
 		e: [2,7]
 	},
-	Neon: {
+	{
+		p: "Neon",
 		n: 10,
 		s: 'Ne',
 		w: 20.1797,
 		c: 'ng',
 		e: [2,8]
 	},
-	Sodium: {
+	{
+		p: "Sodium",
 		n: 11,
 		s: 'Na',
 		w: 22.98976928,
 		c: 'am',
 		e: [2,8,1]
 	},
-	Magnesium: {
+	{
+		p: "Magnesium",
 		n: 12,
 		s: 'Mg',
 		w: 24.305,
 		c: 'aem',
 		e: [2,8,2]
 	},
-	Aluminium: {
+	{
+		p: "Aluminium",
 		n: 13,
 		s: 'Al',
 		w: 26.9815386,
 		c: 'ptm',
 		e: [2,8,3]
 	},
-	Silicon: {
+	{
+		p: "Silicon",
 		n: 14,
 		s: 'Si',
 		w: 28.0855,
 		c: 'me',
 		e: [2,8,4]
 	},
-	Phosphorus: {
+	{
+		p: "Phosphorus",
 		n: 15,
 		s: 'P',
 		w: 30.973762,
 		c: 'onm',
 		e: [2,8,5]
 	},
-	Sulfur: {
+	{
+		p: "Sulfur",
 		n: 16,
 		s: 'S',
 		w: 32.065,
 		c: 'onm',
 		e: [2,8,6]
 	},
-	Chlorine: {
+	{
+		p: "Chlorine",
 		n: 17,
 		s: 'Cl',
 		w: 35.453,
 		c: 'ha',
 		e: [2,8,7]
 	},
-	Argon: {
+	{
+		p: "Argon",
 		n: 18,
 		s: 'Ar',
 		w: 39.948,
 		c: 'ng',
 		e: [2,8,8]
 	},
-	Potassium: {
+	{
+		p: "Potassium",
 		n: 19,
 		s: 'K',
 		w: 39.0983,
 		c: 'am',
 		e: [2,8,8,1]
 	},
-	Calcium: {
+	{
+		p: "Calcium",
 		n: 20,
 		s: 'Ca',
 		w: 40.078,
 		c: 'aem',
 		e: [2,8,8,2]
 	},
-	Scandium: {
+	{
+		p: "Scandium",
 		n: 21,
 		s: 'Sc',
 		w: 44.955912,
 		c: 'tm',
 		e: [2,8,9,2]
 	},
-	Titanium: {
+	{
+		p: "Titanium",
 		n: 22,
 		s: 'Ti',
 		w: 47.867,
 		c: 'tm',
 		e: [2,8,10,2]
 	},
-	Vanadium: {
+	{
+		p: "Vanadium",
 		n: 23,
 		s: 'V',
 		w: 50.9415,
 		c: 'tm',
 		e: [2,8,11,2]
 	},
-	Chromium: {
+	{
+		p: "Chromium",
 		n: 24,
 		s: 'Cr',
 		w: 51.9961,
 		c: 'tm',
 		e: [2,8,13,1]
 	},
-	Manganese: {
+	{
+		p: "Manganese",
 		n: 25,
 		s: 'Mn',
 		w: 54.938045,
 		c: 'tm',
 		e: [2,8,13,2]
 	},
-	Iron: {
+	{
+		p: "Iron",
 		n: 26,
 		s: 'Fe',
 		w: 55.845,
 		c: 'tm',
 		e: [2,8,14,2]
 	},
-	Cobalt: {
+	{
+		p: "Cobalt",
 		n: 27,
 		s: 'Co',
 		w: 58.933195,
 		c: 'tm',
 		e: [2,8,15,2]
 	},
-	Nickel: {
+	{
+		p: "Nickel",
 		n: 28,
 		s: 'Ni',
 		w: 58.6934,
 		c: 'tm',
 		e: [2,8,16,2]
 	},
-	Copper: {
+	{
+		p: "Copper",
 		n: 29,
 		s: 'Cu',
 		w: 63.546,
 		c: 'tm',
 		e: [2,8,18,1]
 	},
-	Zinc: {
+	{
+		p: "Zinc",
 		n: 30,
 		s: 'Zn',
 		w: 65.38,
 		c: 'tm',
 		e: [2,8,18,2]
 	},
-	Gallium: {
+	{
+		p: "Gallium",
 		n: 31,
 		s: 'Ga',
 		w: 69.723,
 		c: 'ptm',
 		e: [2,8,18,3]
 	},
-	Germanium: {
+	{
+		p: "Germanium",
 		n: 32,
 		s: 'Ge',
 		w: 72.63,
 		c: 'me',
 		e: [2,8,18,4]
 	},
-	Arsenic: {
+	{
+		p: "Arsenic",
 		n: 33,
 		s: 'As',
 		w: 74.9216,
 		c: 'me',
 		e: [2,8,18,5]
 	},
-	Selenium: {
+	{
+		p: "Selenium",
 		n: 34,
 		s: 'Se',
 		w: 78.96,
 		c: 'onm',
 		e: [2,8,18,6]
 	},
-	Bromine: {
+	{
+		p: "Bromine",
 		n: 35,
 		s: 'Br',
 		w: 79.904,
 		c: 'ha',
 		e: [2,8,18,7]
 	},
-	Krypton: {
+	{
+		p: "Krypton",
 		n: 36,
 		s: 'Kr',
 		w: 83.798,
 		c: 'ng',
 		e: [2,8,18,8]
 	},
-	Rubidium: {
+	{
+		p: "Rubidium",
 		n: 37,
 		s: 'Rb',
 		w: 85.4678,
 		c: 'am',
 		e: [2,8,18,8,1]
 	},
-	Strontium: {
+	{
+		p: "Strontium",
 		n: 38,
 		s: 'Sr',
 		w: 87.62,
 		c: 'aem',
 		e: [2,8,18,8,2]
 	},
-	Yttrium: {
+	{
+		p: "Yttrium",
 		n: 39,
 		s: 'Y',
 		w: 88.90585,
 		c: 'tm',
 		e: [2,8,18,9,2]
 	},
-	Zirconium: {
+	{
+		p: "Zirconium",
 		n: 40,
 		s: 'Zr',
 		w: 91.224,
 		c: 'tm',
 		e: [2,8,18,10,2]
 	},
-	Niobium: {
+	{
+		p: "Niobium",
 		n: 41,
 		s: 'Nb',
 		w: 92.90638,
 		c: 'tm',
 		e: [2,8,18,12,1]
 	},
-	Molybdenum: {
+	{
+		p: "Molybdenum",
 		n: 42,
 		s: 'Mo',
 		w: 95.96,
 		c: 'tm',
 		e: [2,8,18,13,1]
 	},
-	Technetium: {
+	{
+		p: "Technetium",
 		n: 43,
 		s: 'Tc',
 		w: (98),
 		c: 'tm',
 		e: [2,8,18,13,2]
 	},
-	Ruthenium: {
+	{
+		p: "Ruthenium",
 		n: 44,
 		s: 'Ru',
 		w: 101.07,
 		c: 'tm',
 		e: [2,8,18,15,1]
 	},
-	Rhodium: {
+	{
+		p: "Rhodium",
 		n: 45,
 		s: 'Rh',
 		w: 102.9055,
 		c: 'tm',
 		e: [2,8,18,16,1]
 	},
-	Palladium: {
+	{
+		p: "Palladium",
 		n: 46,
 		s: 'Pd',
 		w: 106.42,
 		c: 'tm',
 		e: [2,8,18,18]
 	},
-	Silver: {
+	{
+		p: "Silver",
 		n: 47,
 		s: 'Ag',
 		w: 107.8682,
 		c: 'tm',
 		e: [2,8,18,18,1]
 	},
-	Cadmium: {
+	{
+		p: "Cadmium",
 		n: 48,
 		s: 'Cd',
 		w: 112.411,
 		c: 'tm',
 		e: [2,8,18,18,2]
 	},
-	Indium: {
+	{
+		p: "Indium",
 		n: 49,
 		s: 'In',
 		w: 114.818,
 		c: 'ptm',
 		e: [2,8,18,18,3]
 	},
-	Tin: {
+	{
+		p: "Tin",
 		n: 50,
 		s: 'Sn',
 		w: 118.71,
 		c: 'ptm',
 		e: [2,8,18,18,4]
 	},
-	Antimony: {
+	{
+		p: "Antimony",
 		n: 51,
 		s: 'Sb',
 		w: 121.76,
 		c: 'me',
 		e: [2,8,18,18,5]
 	},
-	Tellurium: {
+	{
+		p: "Tellurium",
 		n: 52,
 		s: 'Te',
 		w: 127.6,
 		c: 'me',
 		e: [2,8,18,18,6]
 	},
-	Iodine: {
+	{
+		p: "Iodine",
 		n: 53,
 		s: 'I',
 		w: 126.90447,
 		c: 'ha',
 		e: [2,8,18,18,7]
 	},
-	Xenon: {
+	{
+		p: "Xenon",
 		n: 54,
 		s: 'Xe',
 		w: 131.293,
 		c: 'ng',
 		e: [2,8,18,18,8]
 	},
-	Caesium: {
+	{
+		p: "Caesium",
 		n: 55,
 		s: 'Cs',
 		w: 132.9054519,
 		c: 'am',
 		e: [2,8,18,18,8,1]
 	},
-	Barium: {
+	{
+		p: "Barium",
 		n: 56,
 		s: 'Ba',
 		w: 137.327,
 		c: 'aem',
 		e: [2,8,18,18,8,2]
 	},
-	Lanthanum: {
+	{
+		p: "Lanthanum",
 		n: 57,
 		s: 'La',
 		w: 138.90547,
 		c: 'la',
 		e: [2,8,18,18,9,2]
 	},
-	Cerium: {
+	{
+		p: "Cerium",
 		n: 58,
 		s: 'Ce',
 		w: 140.116,
 		c: 'la',
 		e: [2,8,18,19,9,2]
 	},
-	Praseodymium: {
+	{
+		p: "Praseodymium",
 		n: 59,
 		s: 'Pr',
 		w: 140.90765,
 		c: 'la',
 		e: [2,8,18,21,8,2]
 	},
-	Neodymium: {
+	{
+		p: "Neodymium",
 		n: 60,
 		s: 'Nd',
 		w: 144.242,
 		c: 'la',
 		e: [2,8,18,22,8,2]
 	},
-	Promethium: {
+	{
+		p: "Promethium",
 		n: 61,
 		s: 'Pm',
 		w: '(145)',
 		c: 'la',
 		e: [2,8,18,23,8,2]
 	},
-	Samarium: {
+	{
+		p: "Samarium",
 		n: 62,
 		s: 'Sm',
 		w: 150.36,
 		c: 'la',
 		e: [2,8,18,24,8,2]
 	},
-	Europium: {
+	{
+		p: "Europium",
 		n: 63,
 		s: 'Eu',
 		w: 151.964,
 		c: 'la',
 		e: [2,8,18,25,8,2]
 	},
-	Gadolinium: {
+	{
+		p: "Gadolinium",
 		n: 64,
 		s: 'Gd',
 		w: 157.25,
 		c: 'la',
 		e: [2,8,18,25,9,2]
 	},
-	Terbium: {
+	{
+		p: "Terbium",
 		n: 65,
 		s: 'Tb',
 		w: 158.92535,
 		c: 'la',
 		e: [2,8,18,27,8,2]
 	},
-	Dysprosium: {
+	{
+		p: "Dysprosium",
 		n: 66,
 		s: 'Dy',
 		w: 162.5,
 		c: 'la',
 		e: [2,8,18,28,8,2]
 	},
-	Holmium: {
+	{
+		p: "Holmium",
 		n: 67,
 		s: 'Ho',
 		w: 164.93032,
 		c: 'la',
 		e: [2,8,18,29,8,2]
 	},
-	Erbium: {
+	{
+		p: "Erbium",
 		n: 68,
 		s: 'Er',
 		w: 167.259,
 		c: 'la',
 		e: [2,8,18,30,8,2]
 	},
-	Thulium: {
+	{
+		p: "Thulium",
 		n: 69,
 		s: 'Tm',
 		w: 168.93421,
 		c: 'la',
 		e: [2,8,18,31,8,2]
 	},
-	Ytterbium: {
+	{
+		p: "Ytterbium",
 		n: 70,
 		s: 'Yb',
 		w: 173.054,
 		c: 'la',
 		e: [2,8,18,32,8,2]
 	},
-	Lutetium: {
+	{
+		p: "Lutetium",
 		n: 71,
 		s: 'Lu',
 		w: 174.9668,
 		c: 'la',
 		e: [2,8,18,32,9,2]
 	},
-	Hafnium: {
+	{
+		p: "Hafnium",
 		n: 72,
 		s: 'Hf',
 		w: 178.49,
 		c: 'tm',
 		e: [2,8,18,32,10,2]
 	},
-	Tantalum: {
+	{
+		p: "Tantalum",
 		n: 73,
 		s: 'Ta',
 		w: 180.94788,
 		c: 'tm',
 		e: [2,8,18,32,11,2]
 	},
-	Tungsten: {
+	{
+		p: "Tungsten",
 		n: 74,
 		s: 'W',
 		w: 183.84,
 		c: 'tm',
 		e: [2,8,18,32,12,2]
 	},
-	Rhenium: {
+	{
+		p: "Rhenium",
 		n: 75,
 		s: 'Re',
 		w: 186.207,
 		c: 'tm',
 		e: [2,8,18,32,13,2]
 	},
-	Osmium: {
+	{
+		p: "Osmium",
 		n: 76,
 		s: 'Os',
 		w: 190.23,
 		c: 'tm',
 		e: [2,8,18,32,14,2]
 	},
-	Iridium: {
+	{
+		p: "Iridium",
 		n: 77,
 		s: 'Ir',
 		w: 192.217,
 		c: 'tm',
 		e: [2,8,18,32,15,2]
 	},
-	Platinum: {
+	{
+		p: "Platinum",
 		n: 78,
 		s: 'Pt',
 		w: 195.084,
 		c: 'tm',
 		e: [2,8,18,32,17,1]
 	},
-	Gold: {
+	{
+		p: "Gold",
 		n: 79,
 		s: 'Au',
 		w: 196.966569,
 		c: 'tm',
 		e: [2,8,18,32,18,1]
 	},
-	Mercury: {
+	{
+		p: "Mercury",
 		n: 80,
 		s: 'Hg',
 		w: 200.59,
 		c: 'tm',
 		e: [2,8,18,32,18,2]
 	},
-	Thallium: {
+	{
+		p: "Thallium",
 		n: 81,
 		s: 'Tl',
 		w: 204.3833,
 		c: 'ptm',
 		e: [2,8,18,32,18,3]
 	},
-	Lead: {
+	{
+		p: "Lead",
 		n: 82,
 		s: 'Pb',
 		w: 207.2,
 		c: 'ptm',
 		e: [2,8,18,32,18,4]
 	},
-	Bismuth: {
+	{
+		p: "Bismuth",
 		n: 83,
 		s: 'Bi',
 		w: 208.9804,
 		c: 'ptm',
 		e: [2,8,18,32,18,5]
 	},
-	Polonium: {
+	{
+		p: "Polonium",
 		n: 84,
 		s: 'Po',
 		w: '(209)',
 		c: 'me',
 		e: [2,8,18,32,18,6]
 	},
-	Astatine: {
+	{
+		p: "Astatine",
 		n: 85,
 		s: 'At',
 		w: '(210)',
 		c: 'ha',
 		e: [2,8,18,32,18,7]
 	},
-	Radon: {
+	{
+		p: "Radon",
 		n: 86,
 		s: 'Rn',
 		w: '(222)',
 		c: 'ng',
 		e: [2,8,18,32,18,8]
 	},
-	Francium: {
+	{
+		p: "Francium",
 		n: 87,
 		s: 'Fr',
 		w: '(223)',
 		c: 'am',
 		e: [2,8,18,32,18,8,1]
 	},
-	Radium: {
+	{
+		p: "Radium",
 		n: 88,
 		s: 'Ra',
 		w: '(226)',
 		c: 'aem',
 		e: [2,8,18,32,18,8,2]
 	},
-	Actinium: {
+	{
+		p: "Actinium",
 		n: 89,
 		s: 'Ac',
 		w: '(227)',
 		c: 'ac',
 		e: [2,8,18,32,18,9,2]
 	},
-	Thorium: {
+	{
+		p: "Thorium",
 		n: 90,
 		s: 'Th',
 		w: 232.03806,
 		c: 'ac',
 		e: [2,8,18,32,18,10,2]
 	},
-	Protactinium: {
+	{
+		p: "Protactinium",
 		n: 91,
 		s: 'Pa',
 		w: 231.03588,
 		c: 'ac',
 		e: [2,8,18,32,20,9,2]
 	},
-	Uranium: {
+	{
+		p: "Uranium",
 		n: 92,
 		s: 'U',
 		w: 238.02891,
 		c: 'ac',
 		e: [2,8,18,32,21,9,2]
 	},
-	Reptunium: {
+	{
+		p: "Reptunium",
 		n: 93,
 		s: 'Np',
 		w: '(237)',
 		c: 'ac',
 		e: [2,8,18,32,22,9,2]
 	},
-	Plutonium: {
+	{
+		p: "Plutonium",
 		n: 94,
 		s: 'Pu',
 		w: '(244)',
 		c: 'ac',
 		e: [2,8,18,32,24,8,2]
 	},
-	Americium: {
+	{
+		p: "Americium",
 		n: 95,
 		s: 'Am',
 		w: '(243)',
 		c: 'ac',
 		e: [2,8,18,32,25,8,2]
 	},
-	Curium: {
+	{
+		p: "Curium",
 		n: 96,
 		s: 'Cm',
 		w: '(247)',
 		c: 'ac',
 		e: [2,8,18,32,25,9,2]
 	},
-	Berkelium: {
+	{
+		p: "Berkelium",
 		n: 97,
 		s: 'Bk',
 		w: '(247)',
 		c: 'ac',
 		e: [2,8,18,32,27,8,2]
 	},
-	Californium: {
+	{
+		p: "Californium",
 		n: 98,
 		s: 'Cf',
 		w: '(251)',
 		c: 'ac',
 		e: [2,8,18,32,28,8,2]
 	},
-	Einsteinium: {
+	{
+		p: "Einsteinium",
 		n: 99,
 		s: 'Es',
 		w: '(252)',
 		c: 'ac',
 		e: [2,8,18,32,29,8,2]
 	},
-	Fermium: {
+	{
+		p: "Fermium",
 		n: 100,
 		s: 'Fm',
 		w: '(257)',
 		c: 'ac',
 		e: [2,8,18,32,30,8,2]
 	},
-	Mendelevium: {
+	{
+		p: "Mendelevium",
 		n: 101,
 		s: 'Md',
 		w: '(258)',
 		c: 'ac',
 		e: [2,8,18,32,31,8,2]
 	},
-	Nobelium: {
+	{
+		p: "Nobelium",
 		n: 102,
 		s: 'No',
 		w: '(259)',
 		c: 'ac',
 		e: [2,8,18,32,32,8,2]
 	},
-	Lawrencium: {
+	{
+		p: "Lawrencium",
 		n: 103,
 		s: 'Lr',
 		w: '(262)',
 		c: 'ac',
 		e: [2,8,18,32,32,8,3]
 	},
-	Rutherfordium: {
+	{
+		p: "Rutherfordium",
 		n: 104,
 		s: 'Rf',
 		w: '(267)',
 		c: 'tm',
 		e: [2,8,18,32,32,10,2]
 	},
-	Dubnium: {
+	{
+		p: "Dubnium",
 		n: 105,
 		s: 'Db',
 		w: '(268)',
 		c: 'tm',
 		e: [2,8,18,32,32,11,2]
 	},
-	Seaborgium: {
+	{
+		p: "Seaborgium",
 		n: 106,
 		s: 'Sg',
 		w: '(271)',
 		c: 'tm',
 		e: [2,8,18,32,32,12,2]
 	},
-	Bohrium: {
+	{
+		p: "Bohrium",
 		n: 107,
 		s: 'Bh',
 		w: (272),
 		c: 'tm',
 		e: [2,8,18,32,32,13,2]
 	},
-	Hassium: {
+	{
+		p: "Hassium",
 		n: 108,
 		s: 'Hs',
 		w: '(270)',
 		c: 'tm',
 		e: [2,8,18,32,32,14,2]
 	},
-	Meitnerium: {
+	{
+		p: "Meitnerium",
 		n: 109,
 		s: 'Mt',
 		w: '(276)',
 		c: 'tm',
 		e: [2,8,18,32,32,15,2]
 	},
-	Darmstadtium: {
+	{
+		p: "Darmstadtium",
 		n: 110,
 		s: 'Ds',
 		w: '(281)',
 		c: 'tm',
 		e: [2,8,18,32,32,17,1]
 	},
-	Roentgenium: {
+	{
+		p: "Roentgenium",
 		n: 111,
 		s: 'Rg',
 		w: '(280)',
 		c: 'tm',
 		e: [2,8,18,32,32,18,1]
 	},
-	Copernicium: {
+	{
+		p: "Copernicium",
 		n: 112,
 		s: 'Cn',
 		w: '(285)',
 		c: 'tm',
 		e: [2,8,18,32,32,18,2]
 	},
-	Ununtrium: {
+	{
+		p: "Ununtrium",
 		n: 113,
 		s: 'Uut',
 		w: '(284)',
 		c: 'ptm',
 		e: [2,8,18,32,32,18,3]
 	},
-	Flerovium: {
+	{
+		p: "Flerovium",
 		n: 114,
 		s: 'Fl',
 		w: '(289)',
 		c: 'ptm',
 		e: [2,8,18,32,32,18,4]
 	},
-	Ununpentium: {
+	{
+		p: "Ununpentium",
 		n: 115,
 		s: 'Uup',
 		w: '(288)',
 		c: 'ptm',
 		e: [2,8,18,32,32,18,5]
 	},
-	Livermorium: {
+	{
+		p: "Livermorium",
 		n: 116,
 		s: 'Lv',
 		w: '(293)',
 		c: 'ptm',
 		e: [2,8,18,32,32,18,6]
 	},
-	Ununseptium: {
+	{
+		p: "Ununseptium",
 		n: 117,
 		s: 'Uus',
 		w: '(294)',
 		c: 'ha',
 		e: [2,8,18,32,32,18,7]
 	},
-	Ununoctium: {
+	{
+		p: "Ununoctium",
 		n: 118,
 		s: 'Uuo',
 		w: '(294)',
 		c: 'ng',
 		e: [2,8,18,32,32,18,8]
 	}
-};
+];
 
 
 
@@ -835,7 +953,8 @@ var table = {
 
 var tpl,
 	li = "<li class='{0}'><span class='symbol'>{1}</span><span class='name'>{2}</span><span class='segment'><span class='f'>#</span> {3}</span><span class='segment'><span class='f'>w</span> {4}</span><span class='segment'><span class='f'>e</span> {5}</span></li>",
-	el;
+	el,
+	list = [];
 
 
 
@@ -862,43 +981,59 @@ String.prototype.high = function(reg){
 // INTERFACE
 // ============
 
-function filter(what,r){
+function filter(what,r,e){
 	tpl = "";
-	for (var prop in table) {
-		el = table[prop];
+
+	if(list.length == 0 || (e && e.keyCode == 8)){
+		listing = table;
+	} else {
+		listing = list;
+		list = [];
+	}
+
+	for (var i = 0, length = listing.length; i < length; i++) {
+
+	el = listing[i];
+
 		if(r && r != ""){
 			var reg = new RegExp(r, "gi");
 			switch(what){
 				case "s":
 					if(el.s.toLowerCase().indexOf(r.toLowerCase()) > -1){
-						tpl += li.format(el.c, el.s, prop, el.n, el.w, el.e); 
+						tpl += li.format(el.c, el.s, el.p, el.n, el.w, el.e);
+						list.push(el);
 					}
 					break;
 				case "p":
-					if(prop.toLowerCase().indexOf(r.toLowerCase()) > -1){
-						tpl += li.format(el.c, el.s, prop.toString().high(reg), el.n, el.w, el.e);
+					if(el.p.toLowerCase().indexOf(r.toLowerCase()) > -1){
+						tpl += li.format(el.c, el.s, el.p.toString().high(reg), el.n, el.w, el.e);
+						list.push(el);
 					}
 					break;
 				case "n":
 					if(el.n.toString().indexOf(r) > -1){
-						tpl += li.format(el.c, el.s, prop, el.n.toString().high(reg), el.w, el.e);
+						tpl += li.format(el.c, el.s, el.p, el.n.toString().high(reg), el.w, el.e);
+						list.push(el);
 					}
 					break;
 				case "w":
 					if(el.w.toString().indexOf(r) > -1){ 
-						tpl += li.format(el.c, el.s, prop, el.n, el.w.toString().high(reg), el.e);
+						tpl += li.format(el.c, el.s, el.p, el.n, el.w.toString().high(reg), el.e);
+						list.push(el);
 					}
 					break;
 				case "e":
 					if(el.e.toString().indexOf(r) > -1){ 
-						tpl += li.format(el.c, el.s, prop, el.n, el.w, el.e.join(",").high(reg));
+						tpl += li.format(el.c, el.s, el.p, el.n, el.w, el.e.join(",").high(reg));
+						list.push(el);
 					}
 					break;
 				default:
 					break;
 			}
 		} else {
-			tpl += li.format(el.c, el.s, prop, el.n, el.w, el.e);
+			tpl += li.format(el.c, el.s, el.p, el.n, el.w, el.e);
+			list = [];
 		}	
 	}
 	pList.innerHTML = tpl;
